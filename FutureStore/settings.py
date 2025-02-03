bf433e9af0b409bb1e88901dfedef79231bd7d47
Vineesh199812 <108455622+Vineesh199812@users.noreply.github.com>
2025-02-03 19:02:50 +0530

    Update settings.py
    Security update

--- FutureStore/settings.py
+++ FutureStore/settings.py
@@ -142,7 +142,7 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT="587"
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER="vineeshm199812@gmail.com"
-EMAIL_HOST_PASSWORD="rfjecwsfkycjlbpp"
+EMAIL_HOST_PASSWORD="apppassword"
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 
 CORS_ALLOWED_ORIGINS = [
@@ -154,5 +154,4 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
 ]
 
-#asftstbzxhxdwrea
-#rfjecwsfkycjlbpp
+
